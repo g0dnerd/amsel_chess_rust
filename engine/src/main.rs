17355eb973cd315f2089a85db5a9c1bdc0d551e1
Paul Kukowski <kukowskipaul@gmail.com>
2024-04-07 23:14:58 +0200

    Created unit tests, for now validating move generation

--- engine/src/main.rs
+++ engine/src/main.rs
@@ -1,8 +1,5 @@
 use std::env;
-
-use types::square::Square;
 use types::position::Position;
-use engine::movegen;
 
 fn main() {
     env::set_var("RUST_BACKTRACE", "1");
@@ -11,14 +8,60 @@ fn main() {
     // Tests the board's print method
     test_pos.print_position();
 
-    // Tests legal moves for a rook on a1 in the initial position
-    let square = Square::A1;
-    let moves = movegen::get_rook_moves_from_position(square, &test_pos);
-    println!("Rook moves for a1 in the initial position: {:?}", moves);
+}
 
-    // Tests legal moves for a bishop on c1 in the initial position
-    let square = Square::C1;
-    let moves = movegen::get_bishop_moves_from_position(square, &test_pos);
-    println!("Bishop moves for c1 in the initial position: {:?}", moves);
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use types::bitboard::BitBoard;
+    use types::square::Square;
+    use engine::movegen;
 
+    #[test]
+    fn moves_rook_b1_initial() {
+        let test_pos = Position::new();
+        let square = Square::B1;
+        let moves = movegen::get_rook_moves_from_position(square, &test_pos);
+        assert_eq!(moves, BitBoard::empty());
+    }
+
+    #[test]
+    fn moves_knight_b1_initial() {
+        let test_pos = Position::new();
+        let square = Square::B1;
+        let moves = movegen::get_knight_moves(square, &test_pos);
+        assert_eq!(moves, BitBoard::from_u64(327680));
+    }
+
+    #[test]
+    fn moves_knight_b8_initial() {
+        let test_pos = Position::new();
+        let square = Square::B8;
+        let moves = movegen::get_knight_moves(square, &test_pos);
+        assert_eq!(moves, BitBoard::from_u64(5497558138880));
+    }
+
+    #[test]
+    fn moves_bishop_c1_initial() {
+        let test_pos = Position::new();
+        let square = Square::C1;
+        let moves = movegen::get_bishop_moves_from_position(square, &test_pos);
+        assert_eq!(moves, BitBoard::empty());
+    }
+
+    #[test]
+    fn moves_queen_d1_initial() {
+        let test_pos = Position::new();
+        let square = Square::D1;
+        let moves = movegen::get_queen_moves_from_position(square, &test_pos);
+        assert_eq!(moves, BitBoard::empty());
+    }
+
+    #[test]
+    fn moves_king_e1_initial() {
+        let test_pos = Position::new();
+        let square = Square::E1;
+        let moves = movegen::get_king_moves(square, &test_pos);
+        assert_eq!(moves, BitBoard::empty());
+    }
 }
